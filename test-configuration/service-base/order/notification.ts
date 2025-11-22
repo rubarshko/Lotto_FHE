9b16d697a4eda34c20c843e806183d39259e456d
rubarshko <rubarshko@users.noreply.github.com>
2025-11-22 11:17:33 +0000

    Add config.sol, notification.ts, static.jsx

---
+++ test-configuration/service-base/order/notification.ts
@@ -0,0 +1,4 @@
+export const config = { runtime: 'edge' };
+
+export const useadapter_statistics = () => { return {}; };
+
---
+++ test-configuration/service-base/order/static.jsx
@@ -0,0 +1,4 @@
+import { useState, useEffect } from 'react';
+
+export const environment = () => { return null; };
+
